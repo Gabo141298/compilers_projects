7d2c9494a0c3e1d2215c5d37dd3ea043515a9c53
CristianRod21 <christian.rodriguezsoto@ucr.ac.cr>
2020-12-03 20:38:17 -0600

    Se elimina código no util
    Y se agrega doc

--- Singularity/src/node/Program.cc
+++ Singularity/src/node/Program.cc
@@ -25,39 +25,35 @@ llvm::Value* Program::codeGen(CodeGenContext& context)
     context.builder.SetInsertPoint(blockMain);
 
     context.pushBlock(nullptr);
+
+    // Creates global variables
     for(size_t index = 0; index < globals.size(); ++index)
 	{
+        // Evaluates the expression
         llvm::Value* expression = globals[index]->assignmentExpr->codeGen(context);
-        // Creates the variable
-        llvm::ConstantInt* const_int_val = context.builder.getInt64(4000);
 
+        // Makes the global variable with 0 as initializer
 		llvm::Value* var = context.module->getOrInsertGlobal(llvm::StringRef(globals[index]->id.name), expression->getType() );
 		llvm::GlobalVariable *gVar = context.module->getNamedGlobal(llvm::StringRef(globals[index]->id.name));
 		gVar->setLinkage(llvm::GlobalValue::ExternalLinkage);
 		gVar->setAlignment(8); 
         gVar->setInitializer(context.builder.getInt64(0));
 
+        // Stores the value resulted from the expression
         llvm::StoreInst* store = context.builder.CreateStore(expression, gVar);
 
+        // Adds to the context variables
         context.insertVar( globals[index]->id.name, gVar);
 	}
 
-
 	for(size_t index = 0; index < functions.size(); ++index)
 		functions[index]->codeGen(context);
-
-	
-        
 
     context.builder.SetInsertPoint(blockMain);
     
 
     context.builder.CreateCall(context.module->getFunction("start"));
-
     context.builder.CreateRet(context.builder.getInt32(0));
-
-
-
 
 	return nullptr;
 }
